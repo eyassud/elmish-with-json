c39ec8fdf5c62de8329073374a8fd109ea208ddf
dan eyassu <danieleyassu@hotmail.com>
2022-09-05 17:02:33 -0400

    Thoth.Json

---
+++ .history/src/App_20220905164713.fs
@@ -0,0 +1,52 @@
+module App
+
+open Elmish
+open Elmish.React
+open Fable.SimpleHttp
+open Feliz
+
+type State =
+  { StoreInfo: Deferred<Result<string, string>> }
+
+type Msg =
+  | LoadStoreInfo of AsyncOperationEvent<Result<string, string>>
+
+let init() =
+  { StoreInfo = HasNotStartedYet }, Cmd.ofMsg (LoadStoreInfo Started)
+
+let update (msg: Msg) (state: State) =
+  match msg with
+  | LoadStoreInfo Started ->
+      let nextState = { state with StoreInfo = InProgress }
+      let loadStoreInfo =
+        async {
+          // simulate delay
+          do! Async.Sleep 1500
+          let! (statusCode, storeInfo) = Http.get "/store.json"
+          if statusCode = 200
+          then return LoadStoreInfo (Finished (Ok storeInfo))
+          else return LoadStoreInfo (Finished (Error "Could not load the store information"))
+        }
+
+      nextState, Cmd.fromAsync loadStoreInfo
+
+  | LoadStoreInfo (Finished result) ->
+      let nextState = { state with StoreInfo = Resolved result }
+      nextState, Cmd.none
+
+let render (state: State) (dispatch: Msg -> unit) =
+  match state.StoreInfo with
+  | HasNotStartedYet -> Html.none
+  | InProgress -> Html.h1 "Loading..."
+  | Resolved (Error errorMsg) ->
+      Html.h1 [
+        prop.style [ style.color.red ]
+        prop.text errorMsg
+      ]
+
+  | Resolved (Ok products) ->
+      Html.pre products
+
+Program.mkProgram init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
